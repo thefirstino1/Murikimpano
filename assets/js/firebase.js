e95e355b00dece1bc0938887d18c144657ad496a
thefirstino1 <gilbertgnbndayi@gmail.com>
2025-07-28 17:51:01 +0200

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -6,16 +6,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.
 const firebaseConfig = {
   apiKey: "AIzaSyBC-bxI0yfqnlp2TyDAYZH0vEYcQH_tDf0",
   authDomain: "murikimpano.firebaseapp.com",
-  databaseURL: "https://murikimpano-default-rtdb.firebaseio.com",
   projectId: "murikimpano",
-  storageBucket: "murikimpano.appspot.com", // Nahinduye hano gato
+  storageBucket: "murikimpano.appspot.com",
   messagingSenderId: "288360394906",
-  appId: "1:288360394906:web:036db545a633f8eba5af23",
-  measurementId: "G-08Q2CKEE87"
+  appId: "1:288360394906:web:036db545a633f8eba5af23"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Tangiza serivisi yo kwiyandikisha hanyuma uyisangize izindi dosiye
-export const auth = getAuth(app);
+// Nta "export" ikirimo. Turahita tuyikoresha.
+const auth = getAuth(app);
