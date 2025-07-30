6e6cfc8170d5ddbf5b53f8f966fcbe9b0a1ea655
thefirstino1 <gilbertgnbndayi@gmail.com>
2025-07-30 08:28:57 +0200

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,27 +1,15 @@
-```js
-// assets/js/firebase.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-
+// Aya ni ya makuru nyayo ya Firebase yawe.
 const firebaseConfig = {
   apiKey: "AIzaSyBC-bxI0yfqnlp2TyDAYZH0vEYcQH_tDf0",
   authDomain: "murikimpano.firebaseapp.com",
+  databaseURL: "https://murikimpano-default-rtdb.firebaseio.com",
   projectId: "murikimpano",
   storageBucket: "murikimpano.appspot.com",
   messagingSenderId: "288360394906",
-  appId: "1:288360394906:web:036db545a633f8eba5af23"
+  appId: "1:288360394906:web:036db545a633f8eba5af23",
+  measurementId: "G-08Q2CKEE87"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Export auth kugira ngo ikoreshwe ahandi
-export const auth = getAuth(app);
-```
-
-Ubwo noneho muri `auth.js` uzajya ukoresha:
-
-```js
-import { auth } from './firebase.js';
-```
+// Guhita itangiza Firebase. Nta kindi iyi dosiye ikora.
+firebase.initializeApp(firebaseConfig);
+```3.  Emeza impinduka (**"Commit changes"**).
